bc568c042bd218a1086b5169ddb53e4b1c701954
KazariEX <1364035137@qq.com>
2022-09-30 03:10:44 +0800

    加入统计字数功能

--- js/Chapter.js
+++ js/Chapter.js
@@ -28,16 +28,6 @@ VolumeName = [
 var index = 0;
 var vol = 0;
 
-function ChapterWrap(type) {
-    //获取下一章节的序号与卷号
-    var gekka = index + (type == 0 ? -1 : 1);
-    var nextvol  = Volume[gekka];
-    while (nextvol >= 10) nextvol = Math.trunc(nextvol / 10);
-
-    //章节跳转
-    window.location.href = "../Volume." + nextvol + "/novel_" + Volume[gekka] + ".html";
-}
-
 $(document).ready(function() {
     //获取章节序号与卷号
     var path = window.location.pathname;
@@ -53,3 +43,28 @@ $(document).ready(function() {
     //引入正文
     $(".MainContent").load("../../template/Chapter.html");
 })
+
+function ChapterWrap(type) {
+    //获取下一章节的序号与卷号
+    var gekka = index + (type == 0 ? -1 : 1);
+    var nextvol  = Volume[gekka];
+    while (nextvol >= 10) nextvol = Math.trunc(nextvol / 10);
+
+    //章节跳转
+    window.location.href = "../Volume." + nextvol + "/novel_" + Volume[gekka] + ".html";
+}
+
+function WordCount() {
+    var lines = document.getElementsByTagName("p");
+    var rubys = document.getElementsByTagName("ruby");
+    var brs = document.getElementsByTagName("br");
+
+    var count = 0;
+    for (let i = 0; i < lines.length; i++) {
+        count += lines[i].innerHTML.length;
+    }
+
+    count -= rubys.length * 42;
+    count -= brs.length * 6;
+    return count;
+}
