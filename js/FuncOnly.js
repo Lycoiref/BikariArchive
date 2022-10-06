b2ba5d98a450f645cea96a34c6743993c08d3281
KazariEX <1364035137@qq.com>
2022-10-06 14:40:06 +0800

    Normal Change

--- js/FuncOnly.js
+++ js/FuncOnly.js
@@ -21,24 +21,3 @@ function SideBarSlide() {
         }
     })
 }
-
-function Gitalk() {
-    const gitalk = new Gitalk({
-        clientID: '3d9c012ca5497fa45f6d', //GitHub Application Client ID
-        clientSecret: '05478b229256c3f656858775c367a48b6fc5021b', //GitHub Application Client Secret
-    
-        repo: 'BikariComment', //仓库名称(GitHub repo)
-        owner: 'MysteryBao37', //仓库拥有者(GitHub repo owner)
-        admin: ['MysteryBao37'],
-        id: "Demo",      // Ensure uniqueness and length less than 50
-        distractionFreeMode: false  // Facebook-like distraction free mode
-    })
-    
-    var comment = document.createElement("div");
-    comment.id = "gitalk-container";
-    
-    var MainContent = document.getElementsByClassName("MainContent")[0];
-    MainContent.appendChild(comment);
-    
-    gitalk.render('gitalk-container');
-}
